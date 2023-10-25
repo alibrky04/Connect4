8b1f86dadff53148e3cf471a7041da720fa8fad2
Ali Berkay Görgülü <118852487+alibrky04@users.noreply.github.com>
2023-10-25 10:17:08 +0300

    Source code added

---
+++ src/Game_Table.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include "Game_Table.h"
+
+#define ROW 6
+#define COLUMN 7
+
+Game_Table::Game_Table() : Table(8, std::vector<int>(9, 0))
+{
+	
+}
+
+std::vector <std::vector <int>> Game_Table::getTable()
+{
+	return Table;
+}
+
+void Game_Table::setTable(std::vector <std::vector <int>> P_Table)
+{
+	Table = P_Table;
+}
+
+int Game_Table::getRowNumber()
+{
+	return ROW;
+}
+
+int Game_Table::getColumnNumber()
+{
+	return COLUMN;
+}
+
+void Game_Table::printTable()
+{
+	for (int j = 1; j <= COLUMN; j++) {
+		std::cout << j << "  ";
+	}
+
+	std::cout << std::endl;
+	
+	for (int j = 1; j <= COLUMN; j++) {
+		std::cout << "-" << "  ";
+	}
+
+	std::cout << std::endl;
+
+	for (int i = 1; i <= ROW; i++) {
+		for (int j = 1; j <= COLUMN; j++) {
+			std::cout << Table[i][j] << "  ";
+		}
+		std::cout << std::endl;
+	}
+}
+
+Game_Table::~Game_Table()
+{
+}
